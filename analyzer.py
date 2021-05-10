393947982bedd444645616be4a8e66070ed3fb05
hyp3 <0xshb@protonmail.com>
2021-05-10 15:02:13 +0300

    added analyzer

---
+++ analyzer.py
@@ -0,0 +1,13 @@
+class yosh():
+    def __init__(self,hehe):
+        self.get_txt(hehe)
+
+
+    def get_txt(self,txt):
+        self.txt = txt
+        self.printer()
+    def printer(self):
+        print("YOSHHHHHHHH BBBBYYYY")
+
+
+
